6d8c152d8926176331333d208d238ee00e1bf0bb
axelgerard <gerardaxel007@gmail.com>
2026-06-10 10:53:31 +0200

    feat: adding profile form

---
+++ src/Form/ProfileType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username')
+            ->add('email')
+            ->add('password')
+            ->add('roles')
+            ->add('isVerified')
+            ->add('mediaType')
+            ->add('progressionAnime')
+            ->add('progressionManga')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
