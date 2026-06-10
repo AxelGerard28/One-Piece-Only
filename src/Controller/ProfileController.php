3cd30a915c65efa1151f1167304d355dc2e1f022
axelgerard <gerardaxel007@gmail.com>
2026-06-10 11:54:01 +0200

    feat: creating profile page and controller

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\ProfileType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -11,12 +14,27 @@ final class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
     #[IsGranted('ROLE_USER')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
-        $userConnected = $this->getUser();
+        /** @var \App\Entity\User $user */
+        $user = $this->getUser();
+
+        $form = $this->createForm(ProfileType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            $this->addFlash('success', 'Profil mis à jour avec succès ! 🏴‍☠️');
+
+            return $this->redirectToRoute('app_profile');
+        }
 
         return $this->render('profile/index.html.twig', [
-            'user' => $userConnected,
+            'user' => $user,
+            'profileForm' => $form->createView(),
+            'createdChannels' => $user->getCreatedChannels(),
         ]);
     }
 }
--- src/Form/ProfileType.php
+++ src/Form/ProfileType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,15 +15,29 @@ class ProfileType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add('email')
-            ->add('password')
-            ->add('roles')
-            ->add('isVerified')
-            ->add('mediaType')
-            ->add('progressionAnime')
-            ->add('progressionManga')
-        ;
+            ->add('mediaType', ChoiceType::class, [
+                'label' => 'Ton format favori',
+                'choices' => [
+                    'Anime' => 'anime',
+                    'Manga' => 'manga',
+                    'Les deux' => 'both'
+                ],
+                'attr' => ['class' => 'form-select']
+            ])
+            ->add('progressionAnime', IntegerType::class, [
+                'label' => 'Ta progression Anime (Épisode)',
+                'required' => false,
+                'attr' => ['class' => 'form-input', 'placeholder' => 'Ex: 1080']
+            ])
+            ->add('progressionManga', IntegerType::class, [
+                'label' => 'Ta progression Manga (Chapitre)',
+                'required' => false,
+                'attr' => ['class' => 'form-input', 'placeholder' => 'Ex: 1110']
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Mettre à jour mon profil',
+                'attr' => ['class' => 'btn-submit']
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
